4cba77a5f384a9b296c53c18b9eeafe3553a92e0
Anushka1979 <117353225+Anushka1979@users.noreply.github.com>
2024-01-11 23:19:02 +0530

    "commit"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { RegisterComponent } from './components/register/register.component';
 import { NavComponent } from './components/nav/nav.component';
 import { Page1Component } from './components/page1/page1.component';
 import { Page1AboutComponent } from './components/about/page1-about/page1-about.component';
+import { Page2Component } from './components/page2/page2.component';
 
 
 export const routes: Routes = [
@@ -26,6 +27,10 @@ export const routes: Routes = [
     {
         path: 'about',
         component: Page1AboutComponent
+    },
+    {
+        path: 'page2',
+        component: Page2Component
     },
     {
         path: '', redirectTo: 'page1', pathMatch: 'full'
