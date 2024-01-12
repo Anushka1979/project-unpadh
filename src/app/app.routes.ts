ba836d958d064a06932f383778c74cf30f1538bc
UjjvalSoni2001 <156178921+UjjvalSoni2001@users.noreply.github.com>
2024-01-12 19:30:58 +0530

    "Contactpagecomplete"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { RegisterComponent } from './components/register/register.component';
 import { NavComponent } from './components/nav/nav.component';
 import { Page1Component } from './components/page1/page1.component';
 import { Page2Component } from './components/page2/page2.component';
+import { HomeComponent } from './components/contact/home/home.component';
 
 export const routes: Routes = [
     {
@@ -28,5 +29,9 @@ export const routes: Routes = [
     },
     {
         path: '', redirectTo: 'page1', pathMatch: 'full'
+    },
+    {
+        path: 'contact',
+        component: HomeComponent
     }
 ];
--- src/app/components/contact/home/home.component.ts
+++ src/app/components/contact/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavComponent } from '../../nav/nav.component';
+import { Page7Component } from '../../page7/page7.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NavComponent,Page7Component],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
