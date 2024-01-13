41f8b49b9c0e4a95200dfbe4cfaa4d400275b12a
Tushar Kumar <93575669+TusharKumar880@users.noreply.github.com>
2024-01-13 20:06:05 +0530

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,41 +7,53 @@ import { Page2Component } from './components/page2/page2.component';
 import { ProgramsHomeComponent } from './components/programs/programs-home/programs-home.component';
 import { ManagementHomeComponent } from './components/programs/management/management-home/management-home.component';
 import { DevelopmentHomeComponent } from './components/programs/Development/development-home/development-home.component';
+import { DataScienceHomeComponent } from './components/programs/Data-Science/data-science-home/data-science-home.component';
+import { AiHomeComponent } from './components/programs/AI/ai-home/ai-home.component';
 
 export const routes: Routes = [
-    {
-        path: 'login',
-        component: LoginComponent
-    },
-    {
-        path: 'register',
-        component: RegisterComponent
-    },
-    {
-        path: 'nav',
-        component: NavComponent
-    },
-    {
-        path: 'page1',
-        component: Page1Component
-    },
-    {
-        path: 'page2',
-        component: Page2Component
-    },
-    {
-        path: '', redirectTo: 'page1', pathMatch: 'full'
-    },
-    {
-        path: 'programs',
-        component: ProgramsHomeComponent
-    },
-    {
-        path: 'managementHome',
-        component: ManagementHomeComponent
-    },
-    {
-        path: 'developmentHome',
-        component: DevelopmentHomeComponent
-    },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'nav',
+    component: NavComponent,
+  },
+  {
+    path: 'page1',
+    component: Page1Component,
+  },
+  {
+    path: 'page2',
+    component: Page2Component,
+  },
+  {
+    path: '',
+    redirectTo: 'page1',
+    pathMatch: 'full',
+  },
+  {
+    path: 'programs',
+    component: ProgramsHomeComponent,
+  },
+  {
+    path: 'managementHome',
+    component: ManagementHomeComponent,
+  },
+  {
+    path: 'developmentHome',
+    component: DevelopmentHomeComponent,
+  },
+  {
+    path: 'dataScienceHome',
+    component: DataScienceHomeComponent,
+  },
+  {
+    path: 'aiHome',
+    component: AiHomeComponent
+  },
 ];
