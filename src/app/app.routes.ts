b6e9283bd388f1cd686a05c5963e8db634ff7d58
Tushar Kumar <93575669+TusharKumar880@users.noreply.github.com>
2024-01-13 20:01:50 +0530

    commit13

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,50 +9,62 @@ import { ProgramsHomeComponent } from './components/programs/programs-home/progr
 import { ManagementHomeComponent } from './components/programs/management/management-home/management-home.component';
 import { DevelopmentHomeComponent } from './components/programs/Development/development-home/development-home.component';
 import { Page1SeriesComponent } from './components/tseries/page1-series/page1-series.component';
+import { DataScienceHomeComponent } from './components/programs/Data-Science/data-science-home/data-science-home.component';
+import { AiHomeComponent } from './components/programs/AI/ai-home/ai-home.component';
 
 
 export const routes: Routes = [
-    {
-        path: 'login',
-        component: LoginComponent
-    },
-    {
-        path: 'register',
-        component: RegisterComponent
-    },
-    {
-        path: 'nav',
-        component: NavComponent
-    },
-    {
-        path: 'page1',
-        component: Page1Component
-    },
-    {
-        path: 'about',
-        component: Page1AboutComponent
-    },
-    {
-        path: 'tseries',
-        component: Page1SeriesComponent
-    },
-    {
-        path: 'page2',
-        component: Page2Component
-    },
-    {
-        path: '', redirectTo: 'page1', pathMatch: 'full'
-    },
-    {
-        path: 'programs',
-        component: ProgramsHomeComponent
-    },
-    {
-        path: 'managementHome',
-        component: ManagementHomeComponent
-    },
-    {
-        path: 'developmentHome',
-        component: DevelopmentHomeComponent
-    },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'nav',
+    component: NavComponent,
+  },
+  {
+    path: 'page1',
+    component: Page1Component,
+  },
+  {
+    path: 'about',
+    component: Page1AboutComponent,
+  },
+  {
+    path: 'tseries',
+    component: Page1SeriesComponent,
+  },
+  {
+    path: 'page2',
+    component: Page2Component,
+  },
+  {
+    path: '',
+    redirectTo: 'page1',
+    pathMatch: 'full',
+  },
+  {
+    path: 'programs',
+    component: ProgramsHomeComponent,
+  },
+  {
+    path: 'managementHome',
+    component: ManagementHomeComponent,
+  },
+  {
+    path: 'developmentHome',
+    component: DevelopmentHomeComponent,
+  },
+  {
+    path: 'dataScienceHome',
+    component: DataScienceHomeComponent,
+  },
+  {
+    path: 'aiHome',
+    component: AiHomeComponent
+  },
 ];
